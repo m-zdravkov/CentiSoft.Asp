f424ded76161a31c4d21a85f502acceb28d20dae
m-zdravkov <1061991@ucn.dk>
2018-03-05 12:14:17 +0100

    Added GenericRepository

---
+++ CentiSoft/Core/DAL/GenericRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DAL
+{
+    /// <summary>
+    /// Executes generic code, takes in DB set
+    /// </summary>
+    class GenericRepository<T>
+    {
+    }
+}
---
+++ CentiSoft/Core/DAL/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DAL
+{
+    public interface IRepository<T>
+    {
+        void Save(T o);
+        T Load(T o);
+        List<T> LoadAll();
+        void Delete(T o);
+    }
+}
